9aeb6ca7b5f3959d9c9fdb3aafefe7988eaaccbf
Karan Jagtiani <karanjagtiani04@gmail.com>
2019-09-23 06:01:09 +0530

    5th Commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = process.env.PORT||3000;
 app.use(express.static('frontend'));
 
 const host = '0.0.0.0';
-const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
     console.log(process.env.PORT);
     console.log("Server Started on Port: Heroku");
