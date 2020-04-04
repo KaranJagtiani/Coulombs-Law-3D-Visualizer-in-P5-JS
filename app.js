0a7c5aeb887c7f488a7850c6ddce862a1c380191
Karan Jagtiani <karanjagtiani04@gmail.com>
2020-04-04 17:26:40 +0530

    Organised files, deleted unnecessary files

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const port = process.env.PORT||3000;
 
 app.use(express.static('frontend'));
 
-const host = '0.0.0.0';
-
 app.listen(port, () => {
     console.log("Server Started on Port:", port);
 });
