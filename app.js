4b4555c35e2e71f5ac73023c9eb355ba4435132f
Karan Jagtiani <karanjagtiani04@gmail.com>
2020-04-24 11:03:59 +0530

    Fixed alignment issue

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 
 const port = process.env.PORT||3000;
 
-const host = "0.0.0.0";
-
 app.use(express.static('frontend'));
+
+const host = '0.0.0.0';
 
 app.listen(port, () => {
     console.log("Server Started on Port:", port);
